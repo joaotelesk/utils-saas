41e18c3dd92543cdb49c472f9fb635c8630cb00e
Joao Teles <joaojoseteles@gmail.com>
2024-09-16 21:04:47 -0300

    :tada: Initial commit

---
+++ config/eslint-config/node.js
@@ -0,0 +1,15 @@
+/** @type {import('eslint').Linter.Config} */
+module.exports = {
+  extends: ['@rocketseat/eslint-config/node'],
+  plugins: ['simple-import-sort'],
+  rules: {
+    'simple-import-sort/imports': 'error',
+    '@typescript-eslint/no-unused-vars': 'warn',
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
+  },
+}
